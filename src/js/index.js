6ad623004628ea7a3d5365eea90c8857c7b3bd05
Anastasia <telnova.anastasia@gmail.com>
2022-08-03 21:43:08 +0300

    fix slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.explore__slider-places', {
+const swiper = new Swiper('#explore__slider-places', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -11,8 +11,8 @@ const swiper = new Swiper('.explore__slider-places', {
   },
 
 });
-console.log('swiper test');
-const backgroundSwiper = new Swiper('.explore__slider-background', {
+
+const backgroundSwiper = new Swiper('#explore__slider-background', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -20,8 +20,10 @@ const backgroundSwiper = new Swiper('.explore__slider-background', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
   },
 
 });
+
+console.log('swiper test');
